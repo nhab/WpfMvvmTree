98c9ecd5cce7a4b17b0e228e7005cac0d1129063
nhab <nima.habib@arianagermany.com>
2023-08-16 15:43:51 +0330

    simplification

--- Model/TreeModel.cs
+++ Model/TreeModel.cs
@@ -11,7 +11,13 @@ namespace WpfMvvmTree
     public class TreeModel
     {
         public string Name { get; set; }
-        public ObservableCollection<TreeModel> Children 
-        { get; set; } = new ObservableCollection<TreeModel>();
+        public ObservableCollection<TreeModel> Children { get; set; } 
+        
+        public TreeModel(string name="") {
+            Children = new ObservableCollection<TreeModel>();
+            if (!string.IsNullOrEmpty(name))
+                Name = name;
+        }
+
     }
 }
--- ViewModel/ViewModel.cs
+++ ViewModel/ViewModel.cs
@@ -14,29 +14,39 @@ namespace WpfMvvmTree
 {
    public class ViewModel
     { 
-        private CollectionViewSource cvs = new CollectionViewSource();
-        private ObservableCollection<TreeModel> col = new ObservableCollection<TreeModel>();
+        private CollectionViewSource cvs;
+        private ObservableCollection<TreeModel> col;
 
         public ICollectionView View { get => cvs.View; }
 
         public ViewModel()
         {
-            string path = @"c:\temp";
-            foreach (var dir in Directory.GetDirectories(path)) 
-                LoadFolder(dir, col);
+            cvs = new CollectionViewSource();
+            col = new ObservableCollection<TreeModel>();
+            //string path = @"c:\temp";
+            //foreach (var dir in Directory.GetDirectories(path)) 
+            //    LoadFolder(dir, col);
+            TreeModel root1 = new TreeModel("Root 1");
+            
+            TreeModel child1=new TreeModel("Child 1");
+            TreeModel child2 = new TreeModel("Child 2");
+            root1.Children.Add(child1) ;
+            root1.Children.Add(child2);
+            col.Add(root1);
+            
             cvs.Source = col;
         }
 
-        private void LoadFolder(string path, ObservableCollection<TreeModel> col)
-        {
-            TreeModel tree = new TreeModel() 
-            { 
-                Name = IO.Path.GetFileNameWithoutExtension(path) 
-            };
-            col.Add(tree);
-            foreach (var dir in Directory.GetDirectories(path)) 
-                LoadFolder(dir, tree.Children);
-        }
+        //private void LoadFolder(string path, ObservableCollection<TreeModel> col)
+        //{
+        //    TreeModel tree = new TreeModel() 
+        //    { 
+        //        Name = IO.Path.GetFileNameWithoutExtension(path) 
+        //    };
+        //    col.Add(tree);
+        //    foreach (var dir in Directory.GetDirectories(path)) 
+        //        LoadFolder(dir, tree.Children);
+        //}
        
     }
 }

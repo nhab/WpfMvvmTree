7fcd2f00ace5800cc57516a4aceea32b52681adf
nhab <nima.habib@arianagermany.com>
2023-08-17 13:40:20 +0330

    coreection

--- ViewModel/ViewModel.cs
+++ ViewModel/ViewModel.cs
@@ -15,46 +15,46 @@ using WpfMvvmTree.Base;
 using IO = System.IO;
 namespace WpfMvvmTree
 {
-   public class ViewModel
-    { 
+    public class ViewModel
+    {
         private CollectionViewSource cvs;
         private ObservableCollection<TreeModel> col;
-        RelayCommand addParentCommand;
 
+        RelayCommand addCommand;
+      
         public ICollectionView View { get => cvs.View; }
 
         public ViewModel()
         {
             cvs = new CollectionViewSource();
             col = new ObservableCollection<TreeModel>();
-      
+
             TreeModel root1 = new TreeModel("Root 1");
-       
-            root1.Children.Add(new TreeModel("Child 1")) ;
+
+            root1.Children.Add(new TreeModel("Child 1"));
             root1.Children.Add(new TreeModel("Child 2"));
             col.Add(root1);
-            
+
             cvs.Source = col;
-           
+
         }
 
 
 
-        public ICommand AddParentCommand
+        public ICommand AddCommand
         {
             get
             {
-                if (addParentCommand == null)
-                    addParentCommand = new RelayCommand(AddParent);
-                return addParentCommand;
+                if (addCommand == null)
+                    addCommand = new RelayCommand(Add);
+                return addCommand;
             }
 
         }
-   
-     
-        private void AddParent(object parameter)
+      
+        private void Add(object parameter)
         {
-            
+
             if (parameter is TreeModel selectedItm)
             {
                 TreeModel newNode = new TreeModel
@@ -68,9 +68,10 @@ namespace WpfMvvmTree
                     if (child.id == selectedItm.id)
                     {
                         child.Children.Add(newNode);
-                    }else
+                    }
+                    else
                     {
-                        if(child.Children.Count>0)
+                        if (child.Children.Count > 0)
                             foreach (TreeModel grandchild in child.Children)
                             {
                                 if (grandchild.id == selectedItm.id)
@@ -81,10 +82,10 @@ namespace WpfMvvmTree
                     }
                 }
 
-              
+
             }
         }
 
-
+     
     }
 }
